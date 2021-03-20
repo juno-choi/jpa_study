30722706b7db05663f25173485a1115642e6e690
juno-choi <ililil9482@naver.com>
2021-03-20 09:11:33 +0900

    JPA 테이블 상속관계 매핑 전략 SINGLE, TABLE PER <설명> SINGLE과 TABLE PER에 따른 장단점과 테이블 구조를 이해

--- src/main/java/com/example/hellojpa/Item.java
+++ src/main/java/com/example/hellojpa/Item.java
@@ -4,10 +4,11 @@ import javax.persistence.*;
 
 @Entity
 //JPA 기본적략은 싱글 테이블 전략이다
-@Inheritance(strategy = InheritanceType.JOINED)
+//싱글 테이블의 장점은 성능이 제일 잘 나올 확률이 높다.
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 //조인전략의 DTYPE이라는 컬럼으로 구분해서 테이블을 짤수 있도록 해주는 어노테이션 name으로 컬럼 이름도 변경할 수 있다. 하지만 DTYPE그대로 쓰느게 다른 개발자가 보기에 편함
 @DiscriminatorColumn(name = "DIS_TYPE")
-public class Item {
+public abstract class Item {
 
     @Id @GeneratedValue
     private Long id;
